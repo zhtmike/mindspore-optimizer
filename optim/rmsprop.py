5328a788024e4524a04ef82b517ace025022796c
Mike Cheung <zhtmike@gmail.com>
2025-07-15 10:54:36 +0800

    refactor rmsprop

--- optim/rmsprop.py
+++ optim/rmsprop.py
@@ -1,19 +1,20 @@
-from typing import List
+from typing import List, Tuple
 
 import mindspore as ms
-import mindspore.nn as nn
-import mindspore.ops as ops
 import mindspore.mint as mint
-import numpy as np
+import mindspore.ops as ops
 from mindspore import Parameter, ParameterTuple, Tensor
+from mindspore.experimental.optim.optimizer import Optimizer
 
 _rmsprop_opt = ops.MultitypeFuncGraph("rmsprop_opt")
 
 
 @_rmsprop_opt.register(
-    "Number",
-    "Number",
-    "Number",
+    "Float",
+    "Float",
+    "Float",
+    "Bool",
+    "Float",
     "Bool",
     "Tensor",
     "Tensor",
@@ -21,42 +22,40 @@ _rmsprop_opt = ops.MultitypeFuncGraph("rmsprop_opt")
     "Tensor",
     "Tensor",
     "Tensor",
-    "Tensor",
-    "Bool",
-    "Bool",
 )
 def _update_run_op(
     alpha: float,
     eps: float,
     momentum: float,
     centered: bool,
-    lr: Tensor,
-    weight_decay: Tensor,
+    weight_decay: float,
+    maximize: bool,
+    lr: Parameter,
     param: Parameter,
     v: Parameter,
     b: Parameter,
     g_ave: Parameter,
     g: Tensor,
-    decay_flag: bool,
-    optim_filter: bool,
 ) -> bool:
-    if not optim_filter:
-        return False
+    if maximize:
+        g = mint.neg(g)
 
-    if decay_flag:
-        g = g + weight_decay * param
+    if weight_decay > 0:
+        g.add_(weight_decay * param)
 
     v_next = mint.lerp(mint.square(g), v, alpha)
 
     g_ave_next = None
     if centered:
         g_ave_next = mint.lerp(g, g_ave, alpha)
-        v_next.add_(-mint.square(g_ave_next))
+        v_next_hat = v_next - mint.square(g_ave_next)
+    else:
+        v_next_hat = v_next
 
     if momentum > 0:
-        g = momentum * b + g / (mint.sqrt(v_next) + eps)
+        g = momentum * b + g / (mint.sqrt(v_next_hat) + eps)
     else:
-        g = g / (mint.sqrt(v_next) + eps)
+        g = g / (mint.sqrt(v_next_hat) + eps)
 
     param.add_(-lr * g)
 
@@ -68,119 +67,105 @@ def _update_run_op(
     return True
 
 
-class RMSprop(nn.Optimizer):
+class RMSprop(Optimizer):
     """Following https://pytorch.org/docs/stable/generated/torch.optim.RMSprop.html"""
 
     def __init__(
         self,
         params: List[Parameter],
-        lr: float = 0.001,
+        lr: float = 0.01,
         alpha: float = 0.99,
         eps: float = 1e-8,
         weight_decay: float = 0.0,
         momentum: float = 0.0,
         centered: bool = False,
+        maximize: bool = False,
     ) -> None:
-        super().__init__(lr, params, weight_decay)
-        self.alpha = alpha
-        self.eps = eps
-        self.momentum = momentum
-        self.centered = centered
-        self.moments2 = ParameterTuple(
-            [
-                Parameter(np.zeros(x.shape, dtype=np.float32), name="v." + x.name)
-                for x in self._parameters
-            ]
+        defaults = dict(
+            lr=lr,
+            momentum=momentum,
+            alpha=alpha,
+            eps=eps,
+            centered=centered,
+            weight_decay=weight_decay,
+            maximize=maximize,
         )
-        if self.momentum > 0:
-            self.moments1 = ParameterTuple(
-                [
-                    Parameter(np.zeros(x.shape, dtype=np.float32), name="b." + x.name)
-                    for x in self._parameters
-                ]
+        super(RMSprop, self).__init__(params, defaults)
+
+        self.square_avg = self.parameters.clone("square_avg", init="zeros")
+        if momentum > 0:
+            self.momentum_buffer = self.parameters.clone(
+                prefix="momentum_buffer", init="zeros"
             )
         else:
-            self.moments1 = ParameterTuple(
-                [Parameter([], name="b." + x.name) for x in self._parameters]
+            self.momentum_buffer = ParameterTuple(
+                [
+                    Parameter([], name="momentum_buffer ." + x.name)
+                    for x in self.parameters
+                ]
             )
 
-        if self.centered:
-            self.gradient_ave = ParameterTuple(
-                [
-                    Parameter(
-                        np.zeros(x.shape, dtype=np.float32), name="g_ave." + x.name
-                    )
-                    for x in self._parameters
-                ]
-            )
+        if centered:
+            self.grad_avg = self.parameters.clone("grad_avg", init="zeros")
         else:
-            self.gradient_ave = ParameterTuple(
-                [Parameter([], name="g_ave." + x.name) for x in self._parameters]
+            self.grad_avg = ParameterTuple(
+                [Parameter([], name="grad_avg." + x.name) for x in self.parameters]
             )
+
+        self.state_step = Parameter(Tensor(0, dtype=ms.int32))
+        self.increase_tensor = Tensor(1, dtype=ms.int32)
 
     @ms.jit
-    def construct(self, gradients: List[Tensor]) -> bool:
-        weight_decay = self.get_weight_decay()
-        lr = self.get_lr()
-        self.assignadd(self.global_step, self.global_step_increase_tensor)
+    def rmsprop(
+        self,
+        alpha: float,
+        eps: float,
+        momentum: float,
+        centered: bool,
+        weight_decay: float,
+        maximize: bool,
+        lr: Parameter,
+        gradients: Tuple[Tensor],
+        start_id: int,
+        end_id: int,
+    ) -> bool:
+        optim_result = self.hyper_map(
+            ops.partial(
+                _rmsprop_opt,
+                alpha,
+                eps,
+                momentum,
+                centered,
+                weight_decay,
+                maximize,
+                lr,
+            ),
+            self.parameters[start_id:end_id],
+            self.square_avg[start_id:end_id],
+            self.momentum_buffer[start_id:end_id],
+            self.grad_avg[start_id:end_id],
+            gradients[start_id:end_id],
+        )
+        return optim_result
 
-        if self.is_group:
-            if self.is_group_lr:
-                optim_result = self.hyper_map(
-                    ops.partial(
-                        _rmsprop_opt,
-                        self.alpha,
-                        self.eps,
-                        self.momentum,
-                        self.centered,
-                    ),
-                    lr,
-                    weight_decay,
-                    self._parameters,
-                    self.moments2,
-                    self.moments1,
-                    self.gradient_ave,
-                    gradients,
-                    self.decay_flags,
-                    self.optim_filter,
-                )
-            else:
-                optim_result = self.hyper_map(
-                    ops.partial(
-                        _rmsprop_opt,
-                        self.alpha,
-                        self.eps,
-                        self.momentum,
-                        self.centered,
-                        lr,
-                    ),
-                    weight_decay,
-                    self._parameters,
-                    self.moments2,
-                    self.moments1,
-                    self.gradient_ave,
-                    gradients,
-                    self.decay_flags,
-                    self.optim_filter,
-                )
-        else:
-            optim_result = self.hyper_map(
-                ops.partial(
-                    _rmsprop_opt,
-                    self.alpha,
-                    self.eps,
-                    self.momentum,
-                    self.centered,
-                    lr,
-                    weight_decay,
-                ),
-                self._parameters,
-                self.moments2,
-                self.moments1,
-                self.gradient_ave,
+    def construct(self, gradients: Tuple[Tensor]) -> bool:
+        self.state_step += self.increase_tensor
+
+        for group_id, group in enumerate(self.param_groups):
+            start_id = self.group_start_id[group_id]
+            end_id = self.group_start_id[group_id + 1]
+
+            self.rmsprop(
+                group["alpha"],
+                group["eps"],
+                group["momentum"],
+                group["centered"],
+                group["weight_decay"],
+                group["maximize"],
+                group["lr"],
                 gradients,
-                self.decay_flags,
-                self.optim_filter,
+                start_id,
+                end_id,
             )
 
-        return optim_result
+        return True
